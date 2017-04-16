044e6dfdd8076bc61c35acaf313b0cca4b30798b
Federico Dente <dentefederico@live.com>
2017-04-16 15:42:39 +0200

    improved functionality + graphic update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,36 @@
+    /*
+
+
     var myBtn = document.getElementsByClassName('cbutton');
     var myOutput = document.getElementById('output');
     var myCal = "";
     var myCom = false;
+    var mySwitch = false;
+    var myOperators = ["+", "-", "*", "/"];
 
     for(var i=0; i < myBtn.length; i++) {
         myBtn[i].addEventListener('click', function() {
             var myValue = this.innerHTML;
+
             //console.log(myValue);
-            
             if(myCom || myCal == "0") {
                 myCom = false;
                 myCal = "";
             }
+
+            if(myValue == "+" || myValue == "-" || myValue == "*" || myValue == "/") {
+                if(mySwitch) {
+                mySwitch = false;
+                if(myOperators.indexOf(myOutput.innerHTML.slice(-1)) > -1) {
+                    myCal = myCal.substring(0, myCal.length -1);
+                } else {
+                    myCal = eval(myCal);
+                }
+                
+            }
+                mySwitch = true;
+            } 
+
 
             if(myValue == "="){
                 myCal = eval(myCal);
@@ -24,7 +43,8 @@
                 myCal = myCal + myValue;
                 //console.log(myCal);
             }
-           
+
+
             myOutput.innerHTML = myCal;
 
         });
@@ -33,5 +53,50 @@
 
 
 
+*/ 
 
+        var mybtn = document.getElementsByClassName("cbutton");
+        var myOutput = document.getElementById("output");
+        var line = document.getElementById('calc-order')
+        var myCal = '';
+        var myCom = false;
+        var mySwitch = false;
+        var myOpe = ["+", "-", "*", "/"];
+        //console.log(mybtn);
+        for (var i = 0; i < mybtn.length; i++) {
+            mybtn[i].addEventListener("click", function () {
+                var myValue = this.innerHTML;
+                if (myCom || myCal == "0") {
+                    myCom = false;
+                    myCal = '';
+                }
+                if (myValue == "+" || myValue == "-" || myValue == "*" || myValue == "/") {
+                    if (mySwitch) {
+                        mySwitch = false;
+                        if (myOpe.indexOf(myOutput.innerHTML.slice(-1)) > -1) {
+                            myCal = myCal.substring(0, myCal.length - 1);
+                        } else {
+                            myCal = eval(myCal);
+                        }
+                    }
+                    mySwitch = true;
+                }
+                if (myValue == "=") {
+                    myValue = '';
+                    if (myOpe.indexOf(myOutput.innerHTML.slice(-1)) == -1) {
+                        myCal = eval(myCal);
+                    }
+                } else if (myValue == "AC") {
+                    myCal = 0;
+                    myCom = true;
+                } else if (myOutput.innerHTML.indexOf(".") > -1 && myValue == ".") {
+                    myValue = '';
+                } else {
+                    myCal = myCal + myValue;
+                    line.innerHTML = myCal;
+                    console.log(myCal);
+                }
+                myOutput.innerHTML = myCal;
+            });
+        }
 
